3460bfd848200b97eab2150e75ed0b7cbd815f4d
jjccCraft <jjcc201@exeter.ac.uk>
2018-03-07 19:44:47 +0000

    Code comments

--- src/speech/SpeechMode.java
+++ src/speech/SpeechMode.java
@@ -18,7 +18,7 @@ import javax.swing.border.LineBorder;
  *   - Utilises veritcal menus over buttons
  *   - LanguageMenu (known before as LanguageButton) now has more attributes regarding other text-to-speech parameters
  *
- * Conversion to a project-wide MVC planned for the third scrum
+ * Conversion to a project-wide MVC planned for the third scrum, as well as movement to a single directory
  */
 public class SpeechMode extends JFrame implements MenuState
 {
