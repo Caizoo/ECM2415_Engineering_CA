d3f30f968236b249e803df7f12d8dbdbcdae8bf9
WellsRob <32956625+WellsRob@users.noreply.github.com>
2018-03-20 10:44:36 +0000

    Added where to state from Rob

--- src/model/ModelTripComputer.java
+++ src/model/ModelTripComputer.java
@@ -1,25 +1,15 @@
 package model;
 
 public class ModelTripComputer {
-    private double currentLat = 360.00;//impossible value to tell its the inital latitiude
-    private double currentLong = 360.00;//impossible value to tell its the inital longitiude
-    private static double currentTime;
-    private double totalDistance = 0;
-    private double speed =0;
+
 
     public ModelTripComputer(){}
-    public Double deg2rad(Double deg) {
+    public static Double deg2rad(Double deg) {
         return deg * (Math.PI / 180);
     }
 
-    public void setVals(String latitude, String longitude, String speed, String time) {
-        this.currentLat = Double.parseDouble(latitude);
-        this.currentLong = Double.parseDouble(longitude);
-        this.currentTime= Double.parseDouble(time);
-        this.speed= Double.parseDouble(speed);
-    }
 
-    public double getDistance(Double lat1, Double lon1, Double lat2, Double lon2) {
+    public static double getDistance(Double lat1, Double lon1, Double lat2, Double lon2) {
         int r = 6371; // Radius of the earth in km
         double dLat = deg2rad(lat2 - lat1);  // deg2rad below
         double dLon = deg2rad(lon2 - lon1);
@@ -32,18 +22,5 @@ public class ModelTripComputer {
     }
 
 
-    public void setvalues(String latitude, String longitude, String speed, String time) {
-        if (latitude.equals("") || longitude.equals("")) {
 
-        } else {
-            if (this.currentLat == 360.00 || this.currentLong == 360.00) {
-                setVals(latitude, longitude,speed,time);
-            }
-            double distance = getDistance(currentLat, currentLong, Double.parseDouble(latitude), Double.parseDouble(longitude));
-            totalDistance += distance;
-
-            setVals(latitude, longitude,speed,time);
-        }
-
-    }
 }
